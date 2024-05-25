02bd2de84b99ef40de5f7fe3832b76b65f2c45f5
Krish <Krishkumer150@gamil.com>
2024-05-25 10:58:24 +0530

    Updated the value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./components/navbar/Nav";
 import Main from "./components/Cards/Main";
+import Cards from "./components/Cards/Cards";
 const App = () => {
   return (
     <>
       <NavBar />
       <Main />
+      <Cards />
     </>
   );
 };
